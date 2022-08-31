8faa5444dda330bd35e10052db0da4a8a0c59aa8
miyeon.yeon <miyeon.yeon@bespinglobal.com>
2022-09-01 00:48:18 +0900

    스프링MVC : viewResolver 예제
    - OldController에 ModelView 리턴하도록 추가
    - 뷰 리졸버 자동 등록을 위한 설정 추가

--- src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -12,6 +12,6 @@ public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
+        return new ModelAndView("new-form"); // 논리적이름만 넣은 것. -> 뷰 리졸버에서 물리적 이름으로 바꾸어야해요
     }
 }
